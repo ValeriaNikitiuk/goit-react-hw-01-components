d347cfd6d0b9668cb5cf4b57f19fee1076df300a
ValeriaOvod <krossovki.ukraine.vzutya@gmail.com>
2023-01-12 13:03:13 +0200

    /

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -3,8 +3,7 @@ import css from './FriendListItem.module.css'
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
     return ( <li className={css.item}>
-        <span className={css.status}>{isOnline}
-           </span>
+        {isOnline ? <span className={css.online}></span>: <span className={css.ofline}></span>}
         <img className={css.avatar} src={avatar } alt={name} width="48" />
         <p className={css.name}>{name}</p>
 </li>)
